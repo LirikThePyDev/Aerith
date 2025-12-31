fa657fdc243aefb359ffda2ab92b312ea2f072d8
LirikThePyDev <lirikthepythondev@gmail.com>
2025-12-31 23:13:13 +0300

    Create __init__.py

--- aerith/__init__.py
+++ aerith/__init__.py
@@ -1,3 +1,11 @@
-from .interpreter import execute_script
+# __init__.py
+from .interpreter import run_async, Env
+import asyncio
 
-__all__ = ['execute_script']
+def execute_script(script_path):
+    with open(script_path, 'r') as f:
+        source = f.read()
+    env = Env()
+    asyncio.run(run_async(source, env))
+
+__all__ = ['execute_script', 'run_async', 'Env']
